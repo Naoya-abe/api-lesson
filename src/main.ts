ccd2fa605953c528e2f4a46c1a6f0b17110bdb80
阿部直也 <n.abe@gemcook.com>
2023-02-23 21:34:47 +0900

    Prismaの設定完了

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // DTO、バリデーションの設定
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  // CORSの設定
+  app.enableCors({ credentials: true, origin: ['http://localhost:3000'] });
+  app.use(cookieParser());
   await app.listen(8080);
 }
 bootstrap();
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
 
+// prismaServiceはPrismaClientにDATABASE_URLを渡すために作成する
+// DBの接続先を教えている
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+  constructor(private readonly config: ConfigService) {
+    super({
+      datasources: {
+        db: { url: config.get('DATABASE_URL') },
+      },
+    });
+  }
+}
